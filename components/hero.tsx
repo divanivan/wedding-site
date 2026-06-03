e63d9ffa6d98d748cadd01f421987c1de6077ebd
divanivan <divanivandivan123@gmail.com>
2026-06-03 23:36:21 +0300

    fix gallery photos

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useEffect, useState } from "react"
-//import Image from "next/image"
+import Image from "next/image"
 
 export function Hero() {
   const [mounted, setMounted] = useState(false)
